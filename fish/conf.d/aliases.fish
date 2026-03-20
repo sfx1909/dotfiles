783ba24f9740ed2fb3252606c963d20f30fe359a
sfx1909 <seanfvdm@gmail.com>
2026-03-20 22:27:39 +0200

    fix: use functions for eza ls override

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -3,7 +3,12 @@ command -q fastfetch && alias neofetch='fastfetch'
 command -q bat && alias cat='bat'
 command -q nvim && alias v='nvim' && alias vi='nvim' && alias vim='nvim' && alias n='nvim'
 command -q zeditor && alias zed='zeditor'
-command -q eza && alias ls='eza --icons' && alias ll='eza --icons -la'
+command -q eza && function ls
+    eza --icons $argv
+end
+command -q eza && function ll
+    eza --icons -la $argv
+end
 
 # --- General Aliases ---
 alias c='clear'
